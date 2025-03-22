e59d0c2b8342a8862bcb30ee92ba63d1e469c10d
Nicholas Griffin <me@nicholasgriffin.co.uk>
2025-03-22 19:01:03 +0000

    feat: adding streaming support for github

--- apps/api/src/providers/github.ts
+++ apps/api/src/providers/github.ts
@@ -4,7 +4,7 @@ import { BaseProvider } from "./base";
 
 export class GithubModelsProvider extends BaseProvider {
 	name = "github-models";
-	supportsStreaming = false;
+	supportsStreaming = true;
 
 	protected validateParams(params: ChatCompletionParameters): void {
 		super.validateParams(params);
