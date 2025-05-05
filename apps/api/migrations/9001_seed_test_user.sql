b868afc8d8ec8c268b356ebbf93d5ba3ad99e7a4
Nicholas Griffin <me@nicholasgriffin.co.uk>
2025-05-05 13:36:30 +0100

    chore: seed user as pro

--- apps/api/migrations/9001_seed_test_user.sql
+++ apps/api/migrations/9001_seed_test_user.sql
@@ -1,7 +1,11 @@
--- Seed default plan
+-- Seed free plan
 INSERT OR IGNORE INTO plans(id, name, description, price, stripe_price_id)
 VALUES ('free', 'Free Plan', 'Default free plan', 0, 'free');
 
+-- Seed pro plan
+INSERT OR IGNORE INTO plans(id, name, description, price, stripe_price_id)
+VALUES ('pro', 'Pro Plan', 'Default pro plan', 8, 'pro');
+
 -- Seed a test user with ID=1
 INSERT OR IGNORE INTO "user"(id, name, email, plan_id)
-VALUES (1, 'Seed User', 'seed@polychat.app', 'free');
+VALUES (1, 'Seed User', 'seed@polychat.app', 'pro');
