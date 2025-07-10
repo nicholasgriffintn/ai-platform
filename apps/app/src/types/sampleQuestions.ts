226137f66dcb44a3290d7e095005bd18fd1b04c5
Nicholas Griffin <me@nicholasgriffin.co.uk>
2025-07-10 22:41:27 +0100

    feat: adding coding sample questions

---
+++ apps/app/src/types/sampleQuestions.ts
@@ -0,0 +1,9 @@
+export interface Question {
+  id: string;
+  text: string;
+  question: string;
+  category: string;
+  expectedAnswer?: string;
+}
+
+export type QuestionPool = Record<string, Omit<Question, "category">[]>;
